a85d3d23bd7c9e26a2f8d4f5c59f115002ac1ebc
Brisso <ba.brisso@duocuc.cl>
2025-11-25 11:27:26 -0300

    se modifican los colores en base a paleta requerida

--- app/src/main/java/com/example/prueba/ui/theme/Color.kt
+++ app/src/main/java/com/example/prueba/ui/theme/Color.kt
@@ -2,21 +2,17 @@ package com.example.prueba.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// --- Colores base ---
-val CreamPrimary = Color(0xFFFFF5E1)  // #FFF5E1
+val CreamPrimary = Color(0xFFFFF5E1)
 val CreamLight = Color(0xFFFFFAF0)
 val CreamDark = Color(0xFFE6DFC9)
 
-// --- Colores de acento ---
-val BrownPrimary = Color(0xFF6D4C41)  // marrón suave
+val BrownPrimary = Color(0xFF6D4C41)
 val BrownLight = Color(0xFF8D6E63)
 val BrownDark = Color(0xFF4E342E)
 
-// --- Colores secundarios ---
 val PeachAccent = Color(0xFFFFC1A1)
 val PinkAccent = Color(0xFFFFB6B9)
 val MintAccent = Color(0xFFC2EABA)
 
-// --- Textos ---
 val TextPrimary = Color(0xFF3E2723)
 val TextSecondary = Color(0xFF6D4C41)
